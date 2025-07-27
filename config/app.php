1d0eb471dc82ab557452409191c0309641f7b711
aaronfaza <meliesmoderno@gmail.com>
2025-07-27 17:46:58 -0500

    nuevo

--- config/app.php
+++ config/app.php
@@ -226,7 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'url' => env('APP_URL', 'https://sistemaunienergia.onrender.com'),
+    
     ],
 
 ];
