6c0ebbe6294fe3b0d4eb81c706b42de30a1d2e0a
aaronfaza <meliesmoderno@gmail.com>
2025-08-31 14:05:34 -0500

    nuevo

--- resources/views/requerimientos/index.blade.php
+++ resources/views/requerimientos/index.blade.php
@@ -402,7 +402,7 @@
         <div class="modal-footer bg-light">
           <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancelar</button>
           <button type="submit" class="btn btn-success">
-            <i class="fas fa-save me-1"></i> GUARDAR
+            <i class="fas fa-save me-1"></i> Guardar
           </button>
         </div>
       </div>
