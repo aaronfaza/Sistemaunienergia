7e324fc537a8e4ab055cdbd405c1c1e9dcd056b0
aaronfaza <meliesmoderno@gmail.com>
2025-10-11 10:36:49 -0500

    nuevosE

--- resources/views/bienvenida.blade.php
+++ resources/views/bienvenida.blade.php
@@ -369,7 +369,11 @@
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
 -->
+<!-- FullCalendar CSS -->
+<link href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.css" rel="stylesheet">
 
+<!-- FullCalendar JS -->
+<script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js"></script>
 <script>
   // Notificaciones: ocultar badge al abrir
   $(function(){ $('#notificacionesDropdown').on('click', function(){ $('#notiBadge').hide(); }); });
