32dda76b29d1a1d6cc4e95590b509d84af79a1de
unknown <Administrator@UNIENERGIA.PE>
2026-07-21 15:35:19 -0500

    Revert "Agrega confirmacion de boletas y rediseno visual claro del modulo"
    This reverts commit 00a52bd85c1c1d1e5f5b9c9a9e9b9c9e9b9c9e9b9.

--- routes/web.php
+++ routes/web.php
@@ -53,9 +53,6 @@ Route::middleware(['auth', 'rol.restriccion'])->group(function () {
         ->only(['index', 'store', 'destroy'])
         ->parameters(['boletas' => 'boleta']);
 
-    Route::patch('/boletas/{boleta}/confirmar', [BoletaController::class, 'confirmar'])
-        ->name('boletas.confirmar');
-
     Route::patch('/reportes/{reporte}/firmar', [ReporteMantenimientoController::class, 'firmarSupervisor'])
         ->name('reportes.firmar');
 
@@ -76,7 +73,7 @@ Route::middleware(['auth', 'rol.restriccion'])->group(function () {
     // Ruta para imprimir PDF de requerimientos
     Route::get('/requerimientos/{id}/pdf', [RequerimientoController::class, 'imprimir'])
     ->name('requerimientos.imprimir');
-
+    
 
     // Inspección vehicular (nuevo caso de uso)
     Route::resource('inspecciones', InspeccionVehicularController::class)
